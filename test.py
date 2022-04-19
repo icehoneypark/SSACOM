51c49ef828855adc2bb26a3e2ca038453092f8e3
서지원 <yogjesi@naver.com>
2022-04-19 17:50:44 +0900

    skeleton_backend

--- test.py
+++ test.py
@@ -12,13 +12,9 @@ print(456)
 
 
 ## 잘 해보아요 여러분 ##
-<<<<<<< HEAD
 letter = '즐거운 자율 프로젝트'
 
 
 ## 과연 삭제가 될까?
 apple = 'my favorite'
 please = 'dont delete'
-=======
-letter = '즐거운 자율 프로젝트'
->>>>>>> 5d272b40c1cf79860fb4700cbc4734cdf3dab8f7
