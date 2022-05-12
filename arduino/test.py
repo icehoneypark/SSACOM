33eb5b82a4939ce004e73026fad9a0e0ee87066a
김준하 <kjhkjs93@naver.com>
2022-05-12 14:50:47 +0900

    ENG : 데이터 전송용 웹소켓 추가

---
+++ arduino/test.py
@@ -0,0 +1,43 @@
+import asyncio
+import json
+from time import sleep
+# 웹 소켓 모듈을 선언한다.
+import websockets
+
+# **** 실행전에 터미널창에 [ docker run -p 6379:6379 -d redis:2.8 ] 입력 ****
+
+
+# ----------------------------------------------------------------------------------------------//
+# Python 웹소켓 서버에 접속에서 프롬프트 상에서 문자열을 입력받아 Python 웹소켓 서버에 전송하고
+# 전송한 문자열에 대한 에코를 리턴 받는다.
+# quit(종료) 문자를 입력받을 때까지 계속 웹소켓 연결되어 있다. quit 문자가 입력되면 접속이 자동으로 끊긴다.
+# Python 웹소켓 서버는 파이참에서 실행중이다.
+# ----------------------------------------------------------------------------------------------//
+
+async def connect():
+
+    # 웹 소켓에 접속.
+    room_name = input('접속할 방의 이름 : ')
+    async with websockets.connect("ws://localhost:8000/ws/" + room_name +"/") as websocket:
+
+        str = input('웹소켓으로 전송할 내용을 입력[종료하려면 quit 입력!]: ')     # 사용자의 입력을 변수에 저장
+        #print(str)  # 변수의 내용을 출력
+
+        while str != 'quit':
+
+            # quit가 들어오기 전까지 계속 입력받은 문자열을 전송하고 에코를 수신
+            await websocket.send(json.dumps({'message': str}))
+            sleep(0.5)
+            # 웹 소켓 서버로 부터 메시지가 오면 콘솔에 출력
+            # data = await websocket.recv()
+            # print(data)
+            # f.write(data)
+
+            # str = input('웹소켓으로 전송할 내용을 입력[종료하려면 quit 입력!]: ')  # 사용자의 입력을 변수에 저장
+            # print(str)  # 변수의 내용을 출력
+
+        # f.close();
+
+
+# 비동기로 서버에 접속한다.
+asyncio.get_event_loop().run_until_complete(connect())
