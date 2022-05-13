8499835c07339cde4571241f58e7fe87999b28da
JYP <wldb0208@naver.com>
2022-05-13 19:26:29 +0900

    ENG : UWB_test.py modified

--- UWB_test.py
+++ UWB_test.py
@@ -5,12 +5,13 @@ import threading
 from sklearn import preprocessing
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import deque
 
 #라인 단위로 데이터 가져올 리스트 변수
 line = list()
 tmp = list()
 
-count = 1
+count = 0
 # packet 정보
 packet = bytearray()
 packet.append(0x53)#STX1:S
@@ -56,31 +57,6 @@ def parsing_data(datas):
     #print(line[20:30])
     #print(line[30:40])
 
-    """
-    for idx in range(40):
-        data1 = datas[idx * 4 + 2] + datas[idx * 4 + 3]
-        data2 = datas[idx * 4] + datas[idx * 4 + 1]
-        data = data1 + data2
-        data = int(data, 16)
-        line.append(data)
-    """
-    '''
-    for idx in range(40):
-        data = datas[idx * 2 + 1] + datas[idx * 2]
-        data = int(data, 16)
-        line.append(data)
-    '''
-    for idx in range(40):
-        data = datas[idx * 2] + datas[idx * 2 + 1]
-        #data = datas[idx * 2 + 1] + datas[idx * 2]
-        data = int(data, 16)
-        line.append(data)
-
-    print(line[:10])
-    print(line[10:20])
-    print(line[20:30])
-    print(line[30:40])
-    
 
     """
     # normalize
@@ -97,6 +73,7 @@ def parsing_data(datas):
     plt.scatter(x_array, y_array)
     plt.show()
     """
+
 #본 쓰레드
 def readThread(ser):
     global line, tmp
@@ -108,84 +85,63 @@ def readThread(ser):
     #ser.flushInput()
     #ser.flushOutput()
     # ser.read_all()
+    # ser.write(packet)
+            
+    data_80 = deque()
+    real_data = list()
+    real_real_data = list()
     ser.write(packet)
-            
+    tmp_data = ser.read(15).hex()
+    print(tmp_data)
+    before = "45"
+
+    real_real_data = list()
+
     while 1:
-        #tmp_data = list(ser.read_all().hex())
-        #ser.write(packet)
-        tmp_data = ser.read_all()
-        #tmp_data = ser.readline()
-        #tmp_data = ser.read(200)
-        print("횟수 : {}, 타입 : {}, 길이 : {}, 데이터 : {}".format(count, type(tmp_data), len(tmp_data), tmp_data))
-        #if 0 < len(tmp_data) < 24:
-        #    print("what? what? what? what? what? what? what? what?")
-        # tmp_data = tmp_data.hex()
-        if len(tmp_data) > 1:
-            tmp_data_fir = list()
-            tmp_data_fin = list()
-            print("start")
-        
-            print("앞", "뒤")
-            for idx in range(int(len(tmp_data) / 2)):
-                print(tmp_data[idx * 2], tmp_data[idx * 2 + 1])
-                tmp_data_fir.append(tmp_data[idx * 2] + tmp_data[idx * 2 + 1])
-            print("end")
-            """
-            for idx in range(int(len(tmp_data_fir) / 2)):
-                tmp_data_fin.append(int(tmp_data_fir[idx * 2 + 1] + tmp_data_fir[idx * 2], 16))
-            print("read data = {}".format(tmp_data_fin))
-            """
-            '''
-            for idx in range(int(len(tmp_data) / 4)):
-                print(int(tmp_data[idx * 4 + 2] + tmp_data[idx * 4 + 3] + tmp_data[idx * 4] + tmp_data[idx * 4 + 1], 16), end=" ")
-            print("")
-            print("end")
-            '''
-        '''
-        dd = list()
-        ddd = list()
-        if len(tmp_data) > 4:
-            for idx in range(int(len(tmp_data)/ 2)):
-                dd.append(tmp_data[idx * 2] + tmp_data[idx * 2 + 1])
-            
-            for idx in range(int(len(dd)/ 2)):
-                ddd.append(int(dd[idx * 2 + 1] + tmp_data[idx * 2], 16))
 
-            for k in range(len(ddd)):
-                if ddd[k] > 10000:
-                    print("fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck fuck")
-            
-            print(ddd)
+        #tmp_data = ser.read().hex()
         
-        '''
-        #tmp.append(tmp_data)
+        # print("count = {}".format(count))
 
-        '''
-        if len(tmp_data) >= 160:
-            parsing_data(tmp_data)
-            del line[:]
-            del tmp[:]
-        '''
-        '''
-        if len(tmp) == 80:
-            parsing_data(tmp)
-            del line[:]
-            del tmp[:]
-            #ser.flush()
-            #ser.flushInput()
-            #ser.flushOutput()
-            ser.read_all()
-        '''
+        # print(tmp_data, end=" ")
         count += 1
-        if count == 30:
+        tmp_data = ser.read(963).hex()
+
+        # real_data = list(tmp_data[4:1924])
+    
+        real_data = list(tmp_data[6:])
+        
+        for idx in range(960):
+            data1 = real_data[idx] + real_data[idx + 1]
+            data2 = real_data[idx + 2] + real_data[idx + 3]
+
+            data = data2 + data1
+            #real_real_data.append(data)
+            real_real_data.append(int(data, 16))
+
+
+        print("========= {} =========".format(count))
+
+        for idx in range(int(len(real_real_data) / 10)):
+            print(real_real_data[idx * 10 : idx * 10 + 10])
+
+        real_real_data = list()
+
+        # ser.read_all()
+        # print(data_960)
+        if len(data_80) > 80:
+            
+            pass
+        """
+
+        """
+        if len(tmp_data) > 1:
+            pass
+
+        # count += 1
+        if count == 10:
             break
         
-        """
-        tmp = list(ser.read(80).hex())
-        # tmp = list(ser.read(40).hex())
-
-        # trash = ser.read(10000)
-        """ 
         #parsing_data(tmp)
 
         # line 변수 초기화
